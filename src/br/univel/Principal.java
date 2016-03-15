35969596cd7ec109b349e0833c5c3d7737859425
Gabriel Pontin <gabrielpontin01@gmail.com>
2016-03-15 20:22:16 -0300

    alteração pedido do cliente.

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -8,7 +8,7 @@ package br.univel;
 public class Principal {
 	
 	public static void main(String[]args){
-		System.out.println("Olá Mundo !");
-		System.out.println("é nóis !");
+		System.out.println("Hello Word !");
+		System.out.println("Itt's we !");
 	}
 }
